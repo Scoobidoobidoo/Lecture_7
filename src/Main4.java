78afb0533f41ad1d66e7e392149ebdf54793e659
Natalie Datsyuk <nataliedatsyuk@gmail.com>
2019-05-19 14:34:57 +0300

    completed tasks

---
+++ src/Main4.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main4 {
+
+    public static void main(String[] args) {
+        ArraysProcessing4 arraysProcessing4 = new ArraysProcessing4();
+        int[] result = arraysProcessing4.reverseArray(new int[] {1,2,3,4,5});
+        System.out.println(Arrays.toString(result));
+    }
+}
